cbe47f189e3679433365f05dae8cecf0c79f04fd
oybek703 <hhoybek@gmail.com>
2022-01-27 19:16:42 +0500

    build updated

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://yoriqulov-cc.herokuapp.com'
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://yoriqulov-cc.herokuapp.com'
+    // baseURL: 'http://localhost:8000'
 })
 
 export default axiosInstance
